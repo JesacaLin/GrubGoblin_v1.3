372ca367b7ad0e94f03eeb991c6f8074c95f6bd5
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2024-05-25 11:43:32 -0400

    Update Review.java with javaDoc notes

--- src/main/java/com/JesacaLin/GrubGoblin_v13/models/Review.java
+++ src/main/java/com/JesacaLin/GrubGoblin_v13/models/Review.java
@@ -5,16 +5,44 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents a review of a deal.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
+    /**
+     * The unique identifier for the review.
+     */
     private int reviewId;
+
+    /**
+     * The unique identifier for the deal that the review is for.
+     */
     private int dealId;
+
+    /**
+     * The star rating given in the review.
+     */
     private double stars;
+
+    /**
+     * The text of the review.
+     */
     private String reviewDescription;
+
+    /**
+     * The username of the user who wrote the review.
+     */
     private String reviewedBy;
+
+    /**
+     * Returns a string representation of the review.
+     *
+     * @return a string representation of the review
+     */
 
     @Override
     public String toString() {

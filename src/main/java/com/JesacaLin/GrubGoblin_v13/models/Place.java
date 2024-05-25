b73f2513356f3fab531b0261a564208e6eb49aaf
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2024-05-25 11:41:01 -0400

    Update Place.java adding javaDoc

--- src/main/java/com/JesacaLin/GrubGoblin_v13/models/Place.java
+++ src/main/java/com/JesacaLin/GrubGoblin_v13/models/Place.java
@@ -5,17 +5,49 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents a place in the system.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Place {
+    /**
+     * The unique identifier for the place.
+     */
     private int placeId;
+
+    /**
+     * The name of the place.
+     */
     private String placeName;
+
+    /**
+     * The address of the place.
+     */
     private String address;
+
+    /**
+     * The latitude of the place's location.
+     */
     private double latitude;
+
+    /**
+     * The longitude of the place's location.
+     */
     private double longitude;
+
+    /**
+     * The Google rating of the place.
+     */
     private double googleRating;
+
+    /**
+     * Returns a string representation of the place.
+     *
+     * @return a string representation of the place
+     */
 
 
     @Override

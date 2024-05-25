8c867b40a25631015c18868804b27d5db9d6be87
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2024-05-25 11:49:08 -0400

    Update User.java with javaDoc notes

--- src/main/java/com/JesacaLin/GrubGoblin_v13/models/User.java
+++ src/main/java/com/JesacaLin/GrubGoblin_v13/models/User.java
@@ -5,12 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents a user in the system.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    /**
+     * The username of the user.
+     */
     private String username;
+
+    /**
+     * The password of the user.
+     */
     private String password;
+
+    /**
+     * The email address of the user.
+     */
     private String email;
 }
